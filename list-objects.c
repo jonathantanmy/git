bfad85445506615386ca49c2129bc27c4156c568
Jonathan Tan <jonathantanmy@google.com>
2017-09-28 17:39:47 -0700

    rev-list: support termination at promisor objects
    Teach rev-list to support termination of an object traversal at any
    object from a promisor remote (whether one that the local repo also has,
    or one that the local repo knows about because it has another promisor
    object that references it).
    
    This will be used subsequently in gc and in the connectivity check used
    by fetch.
    
    For efficiency, if an object is referenced by a promisor object, and is
    in the local repo only as a non-promisor object, object traversal will
    not stop there. This is to avoid building the list of promisor object
    references.
    
    (In list-objects.c, the case where obj is NULL in process_blob() and
    process_tree() do not need to be changed because those happen only when
    there is a conflict between the expected type and the existing object.
    If the object doesn't exist, an object will be synthesized, which is
    fine.)
    
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- list-objects.c
+++ list-objects.c
@@ -7,6 +7,7 @@
 #include "tree-walk.h"
 #include "revision.h"
 #include "list-objects.h"
+#include "packfile.h"
 
 static void process_blob(struct rev_info *revs,
 			 struct blob *blob,
@@ -24,6 +25,13 @@ static void process_blob(struct rev_info *revs,
 		die("bad blob object");
 	if (obj->flags & (UNINTERESTING | SEEN))
 		return;
+	if (!has_object_file(&obj->oid)) {
+		if (revs->exclude_promisor_objects &&
+		    is_promisor_object(&obj->oid)) {
+			return;
+		}
+		/* error message will be reported later */
+	}
 	obj->flags |= SEEN;
 
 	pathlen = path->len;
@@ -77,6 +85,8 @@ static void process_tree(struct rev_info *revs,
 	enum interesting match = revs->diffopt.pathspec.nr == 0 ?
 		all_entries_interesting: entry_not_interesting;
 	int baselen = base->len;
+	int gently = revs->ignore_missing_links ||
+		     revs->exclude_promisor_objects;
 
 	if (!revs->tree_objects)
 		return;
@@ -84,9 +94,13 @@ static void process_tree(struct rev_info *revs,
 		die("bad tree object");
 	if (obj->flags & (UNINTERESTING | SEEN))
 		return;
-	if (parse_tree_gently(tree, revs->ignore_missing_links) < 0) {
+	if (parse_tree_gently(tree, gently) < 0) {
 		if (revs->ignore_missing_links)
 			return;
+		if (revs->exclude_promisor_objects &&
+		    is_promisor_object(&obj->oid)) {
+			return;
+		}
 		die("bad tree object %s", oid_to_hex(&obj->oid));
 	}
 
